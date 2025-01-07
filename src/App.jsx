84d86fbdb7492cf1985f508ed71b758221a96552
Your Name <your.email@example.com>
2025-01-07 23:16:16 +0500

    fix responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,25 @@ import TreatmentsSettings from "./components/pages/TreatmentsSettings";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="inbox" element={<Inbox />} />
-          <Route path="schedule" element={<Schedule />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="settings" element={<Settings />}>
-            {/* Redirect /settings to /settings/treatments */}
-            <Route index element={<Navigate to="treatments" replace />} />
-            {/* <Route path="general" element={<GeneralSettings />} />
-                        <Route path="password" element={<PasswordSettings />} />
-                        <Route path="price" element={<PriceSettings />} /> */}
-            <Route path="treatments" element={<TreatmentsSettings />} />
-          </Route>
-        </Route>
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route path="dashboard" element={<Dashboard />} />
+    //       <Route path="inbox" element={<Inbox />} />
+    //       <Route path="schedule" element={<Schedule />} />
+    //       <Route path="reviews" element={<Reviews />} />
+    //       <Route path="settings" element={<Settings />}>
+    //         {/* Redirect /settings to /settings/treatments */}
+    //         <Route index element={<Navigate to="treatments" replace />} />
+    //         {/* <Route path="general" element={<GeneralSettings />} />
+    //                     <Route path="password" element={<PasswordSettings />} />
+    //                     <Route path="price" element={<PriceSettings />} /> */}
+    //         <Route path="treatments" element={<TreatmentsSettings />} />
+    //       </Route>
+    //     </Route>
+    //   </Routes>
+    // </Router>
+    <Dashboard />
   );
 };
 
